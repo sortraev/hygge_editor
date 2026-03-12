a7f8b3bb8f76cf2b53e205711540bc86f1448741
sortraev <anersholst@gmail.com>
2026-03-12 12:33:12 +0100

    Add EditorState tests and TODOs

--- test/editor_state_tests.c
+++ test/editor_state_tests.c
@@ -6,7 +6,7 @@
 
 int main(void) {
 
-  // empty Lines
+  // init state
   {
     EditorState *state = stateInit();
 
@@ -17,41 +17,50 @@ int main(void) {
     stateFree(state);
   }
 
-  // append empty line
+  // stateAppendEmptyLine
   {
     EditorState *state = stateInit();
 
-    size_t numLines = state->numLines;
     ASSERT_(stateAppendEmptyLine(state) == 0);
     ASSERT_(state->lines != NULL);
-    ASSERT_(state->numLines == numLines + 1);
+    ASSERT_(state->numLines == 1);
 
-    // assert new line has a non-null but empty string
-    ASSERT_(state->lines[0].s != NULL);
-    ASSERT_(state->lines[0].len == 0);
-    ASSERT_(state->lines[0].cap > 0);
+    ASSERT_(stateAppendEmptyLine(state) == 0);
+    ASSERT_(state->numLines == 2);
+
+    // assert new lines have non-null but empty content
+    for (int i = 0; i < state->numLines; i++) {
+      ASSERT_(state->lines[i].s != NULL);
+      ASSERT_(state->lines[i].len == 0);
+      ASSERT_(state->lines[i].cap > 0);
+    }
 
     stateFree(state);
   }
 
-  // insert string in line
+  // stateInsertString
   {
     EditorState *state = stateInit();
 
     ASSERT_(stateAppendEmptyLine(state) == 0);
     ASSERT_(stateInsertString(state, 0, 0, "foo bar") == 0);
+
+    ASSERT_(state->numLines == 1);
+    ASSERT_(state->lines[0].len == 7);
+    ASSERT_(state->lines[0].cap > 7);
     ASSERT_(strcmp(state->lines[0].s, "foo bar") == 0);
 
     stateFree(state);
   }
 
-  // insert string in arbitrary line
+  // stateInsertString in arbitrary line
   {
     EditorState *state = stateInit();
 
     ASSERT_(stateAppendEmptyLine(state) == 0);
     ASSERT_(stateAppendEmptyLine(state) == 0);
     ASSERT_(stateAppendEmptyLine(state) == 0);
+
     ASSERT_(stateInsertString(state, 1, 0, "foo") == 0);
     ASSERT_(stateInsertString(state, 1, 3, " bar") == 0);
 
@@ -64,6 +73,43 @@ int main(void) {
     ASSERT_(strcmp(state->lines[2].s, "") == 0);
 
     stateFree(state);
+  }
+
+  // out-of-bounds stateInsertString
+  {
+    EditorState *state = stateInit();
+
+    size_t numLines = 3;
+    for (int i = 0; i < numLines; i++)
+      ASSERT_(stateAppendEmptyLine(state) == 0);
+
+    // line index out of bounds
+    ASSERT_(stateInsertString(state, numLines + 3, 0, "foo") == 1);
+
+    // col index out of bounds
+    ASSERT_(stateInsertString(state, 2, 13, "foo") == 1);
+
+    // both indices out of bounds
+    ASSERT_(stateInsertString(state, numLines + 3, 13, "foo") == 1);
+
+    // assert no line has been touched
+    for (int i = 0; i < state->numLines; i++) {
+      ASSERT_(state->lines[i].s != NULL);
+      ASSERT_(state->lines[i].len == 0);
+      ASSERT_(strcmp(state->lines[0].s, "") == 0);
+    }
+
+    stateFree(state);
+  }
+
+  // TODO: stateInsertChar
+  // TODO: out-of-bounds stateInsertChar
+
+  // TODO: stateDeleteChar
+  // TODO: out-of-bounds stateDeleteChar
+
+  // TODO: _stateResizeLines
+  {
   }
 
   printf("EditorState tests: Success\n");
