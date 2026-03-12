e6c2b360dab6518c7efd3ae4ac8fb7f29580e21d
sortraev <anersholst@gmail.com>
2026-03-12 23:05:13 +0100

    malloc wrappers + always define _assert

--- src/util.h
+++ src/util.h
@@ -8,7 +8,6 @@
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 
-#ifdef DEBUG
 static void _assert(int cond, const char *fmt, ...) {
   if (!cond) {
     va_list args;
@@ -18,6 +17,8 @@ static void _assert(int cond, const char *fmt, ...) {
     exit(1);
   }
 }
+
+#ifdef DEBUG
 #define ASSERT(cond, msg) \
   do {\
       _assert((cond), "Assertion failed: %s (%s:%d)\n", (msg), __FILE__, __LINE__);\
@@ -31,5 +32,25 @@ static void _assert(int cond, const char *fmt, ...) {
 #define ASSERT_(cond) ASSERT((cond), "Assertion failed: " #cond)
 
 #define NOTNULL_(expr) NOTNULL((expr), #expr " is null")
+
+void *_mallocOrDie(size_t allocSize, char *file, int line) {
+  void *p = malloc(allocSize);
+  _assert(p != NULL, "malloc() error: %s (%s:%d)", strerror(errno), file, line);
+  return p;
+}
+void *_reallocOrDie(void *p, size_t allocSize, char *file, int line) {
+  p = realloc(p, allocSize);
+  _assert(p != NULL, "realloc() error: %s (%s:%d)", strerror(errno), file, line);
+  return p;
+}
+void *_callocOrDie(size_t n, size_t elemSize, char *file, int line) {
+  void *p = calloc(n, elemSize);
+  _assert(p != NULL, "calloc() error: %s (%s:%d)", strerror(errno), file, line);
+  return p;
+}
+
+#define mallocOrDie(allocSize) _mallocOrDie((allocSize), __FILE__, __LINE__)
+#define reallocOrDie(p, allocSize) _reallocOrDie((p), (allocSize), __FILE__, __LINE__)
+#define callocOrDie(n, elemSize) _callocOrDie((n), (elemSize), __FILE__, __LINE__)
 
 #endif // UTIL_H
